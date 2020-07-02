b800ca9d30281b981f45b4c391e96ce74e3ebf46
Vishwa Kamal <vishwakamal@Kavithas-MacBook-Pro-3.local>
2020-07-02 17:29:54 -0400

    Updates

---
+++ precache-manifest.0fe0c8d8c720d9f76c217eadb56c3fb8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a744f888318058914030134aa644489c",
+    "url": "/viskam-app/index.html"
+  },
+  {
+    "revision": "1b938fd57caeaff33ef5",
+    "url": "/viskam-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "564a169b43ae97b16d4e",
+    "url": "/viskam-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1b938fd57caeaff33ef5",
+    "url": "/viskam-app/static/js/2.b10a4d32.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/viskam-app/static/js/2.b10a4d32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "564a169b43ae97b16d4e",
+    "url": "/viskam-app/static/js/main.32d8eda2.chunk.js"
+  },
+  {
+    "revision": "da6e2be5e72a684517ad",
+    "url": "/viskam-app/static/js/runtime-main.4739b067.js"
+  },
+  {
+    "revision": "ad717c3e640a72198b96cca261754150",
+    "url": "/viskam-app/static/media/covid.ad717c3e.png"
+  }
+]);
