cb4e86c595ec05247528d2ccbcc3a312d4025418
Vishwa Kamal <vishwakamal@Kavithas-MacBook-Pro-3.local>
2020-07-02 17:16:44 -0400

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./assets/images/covid.png";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { NavigationBar } from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
+      <React.Fragment>
+        <Router>
+          <NavigationBar />
+        </Router>
+      </React.Fragment>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <p>COVID-19 Relief</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.coronavirus.in.gov"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Official Indiana COVID-19 website
+        </a> */}
       </header>
     </div>
   );
