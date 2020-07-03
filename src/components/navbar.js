adb23a8ff3870825dbff362aba46941d6357d298
Vishwa Kamal <vishwakamal@Kavithas-MacBook-Pro-3.local>
2020-07-02 21:19:30 -0400

    latest

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
 import styled from "styled-components";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { About } from "./about";
 const Styles = styled.div`
   .navbar {
